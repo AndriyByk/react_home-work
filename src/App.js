745f533d564b33ccb74f2ddf50c9575843e76c19
AndriyByk <andrew.byk.t@gmail.com>
2022-02-04 11:13:42 +0200

    some preparation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import './App.css';
-
+// additional part:
 // * реализовать обновление машин
 // ** добавить валидацыю к форме
 // *** добавить еще 3 слайса для users, posts и comments
 //    и реализовать роутинг между переключением по страницам UsersPage, PostsPage, CommentsPage, CarsPage
 //    (c плейсхолдера достаточно просто вывести все по каждой сущности)
+
+import './App.css';
 
 function App() {
   return (
